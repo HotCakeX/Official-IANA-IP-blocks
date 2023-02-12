e5babc8b306b89647a056de685a4b4def71f6f46
innovatodev <innovatodev@gmail.com>
2023-02-12 11:39:33 +0100

    date

--- gh-pages.ps1
+++ gh-pages.ps1
@@ -3,24 +3,23 @@
 # IPV4
 $LIST_IPV4 = @()
 $null = Get-ChildItem .\CIDR-IPAddress\IPV4 -Filter '*.txt' | Select-Object * | ForEach-Object {
-    $Date = Get-Date -UFormat "Y-%Y|M-%m|D-%d|H-%H|M-%M|S-%S"
-    $LIST_IPV4 += "`n[$Date] - <a href=""https://raw.githubusercontent.com/HotCakeX/Official-IANA-IP-blocks/main/CIDR-IPAddress/IPV4/$($_.BaseName).txt"">$($_.BaseName)_ipv4</a><br>`n"
+    $LIST_IPV4 += "`n<a href=""https://raw.githubusercontent.com/HotCakeX/Official-IANA-IP-blocks/main/CIDR-IPAddress/IPV4/$($_.BaseName).txt"">$($_.BaseName)_ipv4</a><br>`n"
 }
 
 # IPV6
 $LIST_IPV6 = @()
 $null = Get-ChildItem .\CIDR-IPAddress\IPV6 -Filter '*.txt' | Select-Object * | ForEach-Object {
-    $Date = Get-Date -UFormat "Y-%Y|M-%m|D-%d|H-%H|M-%M|S-%S"
-    $LIST_IPV6 += "`n[$Date] - <a href=""https://raw.githubusercontent.com/HotCakeX/Official-IANA-IP-blocks/main/CIDR-IPAddress/IPV6/$($_.BaseName).txt"">$($_.BaseName)_ipv6</a><br>`n"
+    $LIST_IPV6 += "`n<a href=""https://raw.githubusercontent.com/HotCakeX/Official-IANA-IP-blocks/main/CIDR-IPAddress/IPV6/$($_.BaseName).txt"">$($_.BaseName)_ipv6</a><br>`n"
 }
 
 # GH BRANCH
 git fetch origin gh-pages
 git checkout gh-pages
 
+$Date = Get-Date -AsUTC -UFormat "Year:%Y|Month:%m|Day:%d|Hour:%H|Minute:%M|Second:%S"
 $content = Get-Content .\index.html -Raw
-$content = $content -replace "(?s)(?<=<!-- Country-IP-List-IPV4:START -->).*(?=<!-- Country-IP-List-IPV4:END -->)", $LIST_IPV4
-$content = $content -replace "(?s)(?<=<!-- Country-IP-List-IPV6:START -->).*(?=<!-- Country-IP-List-IPV6:END -->)", $LIST_IPV6
+$content = $content -replace "(?s)(?<=<!-- Country-IP-List-IPV4:START -->).*(?=<!-- Country-IP-List-IPV4:END -->)", "$Date (UTC) `n $LIST_IPV4"
+$content = $content -replace "(?s)(?<=<!-- Country-IP-List-IPV6:START -->).*(?=<!-- Country-IP-List-IPV6:END -->)", "$Date (UTC) `n $LIST_IPV6"
 $null = Set-Content .\index.html -Value $content.TrimEnd()
 
 # Pushing the sources and list to main
